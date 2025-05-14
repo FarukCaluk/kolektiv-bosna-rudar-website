20b10bae8fca1802df4d7efea525541690122891
Faruk Čaluk <134332855+FarukCaluk@users.noreply.github.com>
2025-05-14 19:26:38 +0200

    design changes and responsive changes

--- src/app/components/LocationSection.tsx
+++ src/app/components/LocationSection.tsx
@@ -8,7 +8,7 @@ interface LocationSectionProps {
 const LocationSection: React.FC<LocationSectionProps> = ({ locations }) => {
   return (
     <div className="bg-gradient z-50-to-t cursor-default from-[#ff8d7cb4] to-[#F45B44] text-white border-y border-[#ff8d7cb4] py-8 px-4 sm:px-8 shadow-md">
-      <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-5 gap-6 items-center justify-items-center text-center">
+      <div className="lg:flex md:flex align-center justify-center gap-16 text-center">
         {locations.map((location, index) => (
           <div key={index} className="flex flex-col items-center justify-center gap-4">
             <span className="text-2xl sm:text-3xl md:text-4xl font-extralight">
